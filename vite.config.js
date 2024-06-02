45121367e33cc30cd33597a38f255a1e6f778a94
Tom <web@tomcanham.co.uk>
2024-06-02 13:49:07 +0100

    Updated the manifest files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } })],
+  plugins: [react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      theme_color: '#0097d5',
+      short_name: 'Battery Calculator App',
+      lang: 'en-GB',
+      orientation: 'portrait',
+    }
+  })],
 })
