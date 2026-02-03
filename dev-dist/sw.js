1b8014110608703a3d3f2d6e36d1875313252f6e
Tom <web@tomcanham.co.uk>
2026-02-03 20:11:38 +0000

    Merged in update/claude

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jvpvn6h3gio"
+    "revision": "0.t22i21fkaro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
